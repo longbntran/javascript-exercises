87a93e943cd0ab226c392d00db30c0c54051add2
longbntran <longbntran@gmail.com>
2023-06-11 15:17:16 +0700

    finish ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times<0) {
+        return "ERROR"
+    }
+    else if (times==0) {
+        return "";
+    }
+    else {
+        return string.repeat(times);
+    }
 };
+// console.log(repeatString("long",5));
 
 // Do not edit below this line
 module.exports = repeatString;
