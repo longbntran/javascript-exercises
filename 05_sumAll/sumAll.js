358bea6f78ce88145820c7a1d3f30f5b5b387277
longbntran <longbntran@gmail.com>
2023-06-11 20:35:42 +0700

    finish ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    if (typeof(firstNum) !== 'number' || typeof(secondNum) !== 'number') {
+        return "ERROR";
+    }
+    else {
+        if ((firstNum<0) || secondNum<0) {
+            return "ERROR";
+        }
+        else {
+            let max = Math.max(firstNum, secondNum);
+            let min = Math.min(firstNum, secondNum);
+            let result = 0;
+            for (let i = min; i < max+1; i++) {
+                result = result+ i;
+            };
+            return result;
+        }
+    }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+// console.log(sumAll(1,4));
