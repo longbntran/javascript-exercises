72d679a1dc7df078820cf94c4a24148f73a70370
longbntran <longbntran@gmail.com>
2023-06-11 20:04:11 +0700

    finish ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, values) {
+    const valArray = Array.isArray(values) ? values : [values];
+    // console.log(valArray);
+    // for (let i = 0; i < array.length; i++) {
+    //     console.log(array[i]);
+    //     console.log(valArray.indexOf(array[i]) === -1);
+    // }
+    const result = array.filter(function(val) { 
+        return valArray.indexOf(val) === -1;
+    });
+    return result;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+// console.log(removeFromArray([1, 2, 3, 4], 3));
