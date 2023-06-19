9c2e2a56b1f72fd645d1fbb3d6579469e0a8dc7d
longbntran <longbntran@gmail.com>
2023-06-19 17:53:22 +0700

    finish ex 11 getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
 const getTheTitles = function() {
-
+    console.log(Array.from(arguments)[0]);
+    let books = [...arguments][0];
+    // console.log(books);
+    let title = books.map(function(book) {
+        return book.title;});
+    // console.log(title);
+    return title;
 };
 
 // Do not edit below this line
