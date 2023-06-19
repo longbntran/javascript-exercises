b811de3da554ddd7e2719fd5102a325adbb1bc53
longbntran <longbntran@gmail.com>
2023-06-19 16:38:05 +0700

    finish ex 8 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
-
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
 const sum = function() {
-	
+  let array = [...arguments];
+  let result = array[0].reduce( function(value,total) {
+    return value + total;
+  }, 0);
+  return result;
 };
-
+console.log(sum([7,11]));
 const multiply = function() {
-
+  let array = Array.from(arguments);
+  let result = array.reduce(function(value, product) {
+    return value*product;
+    }, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  switch (b) {
+    case 0: return 1;
+    case 1: return a;
+    case -1: return 1/a;
+  }
+	if (b<-1) {
+    return a*power(a,b+1);
+  }
+  if (b>1) {
+    return a*power(a,b-1);
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  }
+  return a * factorial(a-1);
 };
 
 // Do not edit below this line
