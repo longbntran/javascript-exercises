388ee194c432eb4b1afdac666bce3a7b70d8bce2
longbntran <longbntran@gmail.com>
2023-06-20 10:02:46 +0700

    finish ex 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
 const findTheOldest = function() {
-
+    const theOldestArray = Array.from(arguments).flat();
+    let theOldest = theOldestArray.reduce ((obj, person) => {
+        const isObjectEmpty = (obj) => {
+            return Object.keys(obj).length === 0
+        }
+        if (isObjectEmpty(obj)) { 
+            return person;
+        }
+        else {
+            if (!person.yearOfDeath) { person.yearOfDeath = new Date().getFullYear(); }
+            if (!obj.yearOfDeath) { obj.yearOfDeath = new Date().getFullYear();}
+            let personYears = person.yearOfDeath - person.yearOfBirth;
+            let objYears = obj.yearOfDeath -obj.yearOfBirth;
+            return personYears>= objYears ? person : obj;
+        } 
+    }, {});
+    return theOldest;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
